5afa0099729dd8f7be5f27f3e8d2a0137a863d1a
ibi <ibrahimhamitouche@hotmail.com>
2023-09-12 22:08:33 -0400

    commit initial, code en MVC, debut entity -Author : Ibrahim Hamitouche

---
+++ critique-continue-_LL_FR_AF_IH/Controlleur/src/main/java/a23/climoilou/mono2/tp1/_LL_IH_FR_AF/CritiqueContinueLlFrAfIhApplication.java
@@ -0,0 +1,13 @@
+package a23.climoilou.mono2.tp1._LL_IH_FR_AF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CritiqueContinueLlFrAfIhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CritiqueContinueLlFrAfIhApplication.class, args);
+    }
+
+}
---
+++ critique-continue-_LL_FR_AF_IH/Modele/src/main/java/a23/climoilou/mono2/tp1/_LL_IH_FR_AF/Type.java
@@ -0,0 +1,7 @@
+package a23.climoilou.mono2.tp1._LL_IH_FR_AF;
+
+public enum Type {
+    Utilisateur,
+    Expert,
+    Influencer
+}
