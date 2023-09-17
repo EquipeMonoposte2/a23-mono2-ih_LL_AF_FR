25d0c71173b94ac70a0a0607710f5a53121d0c8d
antou98 <antou98@live.com>
2023-09-17 15:33:37 -0400

    Commit de la partie vue de navigation et de son controleur. Aussi, ajout la vue d'ajout de nouveau produit et son controleur. Navigation vue permet d'inclure toutes les vues. 2023-09-17 (15:33)

---
+++ critique-continue-_LL_FR_AF_IH/Controlleur/src/main/java/a23/climoilou/mono2/tp1/_LL_IH_FR_AF/vuecontroleurs/NouveauProduitControleur.java
@@ -0,0 +1,38 @@
+package a23.climoilou.mono2.tp1._LL_IH_FR_AF.vuecontroleurs;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * Controleur de la vue NouveauProduitVue.fxml
+ */
+@FxmlView("NouveauProduitVue.fxml")
+@Component
+public class NouveauProduitControleur implements ControleurInterface{
+
+    @FXML
+    private Button buttonCreationInput;
+
+    @FXML
+    private DatePicker dateSortieMediaInput;
+
+    @FXML
+    private TextField descriptionMediaInput;
+
+    @FXML
+    private TextField lienImageMediaInput;
+
+    @FXML
+    private TextField nomMediaInput;
+
+    @FXML
+    void createMedia(ActionEvent event) {
+        System.out.println("Test du fonction du nouveau produit controleur");
+    }
+}
