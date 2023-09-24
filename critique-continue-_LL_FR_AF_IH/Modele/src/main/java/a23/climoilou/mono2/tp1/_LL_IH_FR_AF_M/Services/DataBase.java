92dd7d7d43e3cc329b79d2ffae9ea3307b3e5eee
Ibi121 <2063828@cegeplimoilou.ca>
2023-09-24 14:12:21 -0400

    Ajout du BD service class

--- critique-continue-_LL_FR_AF_IH/Modele/src/main/java/a23/climoilou/mono2/tp1/_LL_IH_FR_AF_M/Services/DataBase.java
+++ critique-continue-_LL_FR_AF_IH/Modele/src/main/java/a23/climoilou/mono2/tp1/_LL_IH_FR_AF_M/Services/DataBase.java
@@ -4,40 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-//@Service
+@Service
 public class DataBase {
+    private CritiquesService serviceCritique;
 
-    /*private CritiquesService critiquesService;
+    private UtilisateursService serviceUser;
 
     private ProduitsService produitsService;
 
-    private UtilisateursService utilisateursService;
-
+    @Autowired
+    public void setServiceCritique(CritiquesService serviceCritique) {
+        this.serviceCritique = serviceCritique;
+    }
 
     @Autowired
-    public void setCritiquesService(CritiquesService critiquesService) {
-        this.critiquesService = critiquesService;
+    public void setServiceUser(UtilisateursService serviceUser) {
+        this.serviceUser = serviceUser;
     }
-
-    public CritiquesService getCritiquesService() {
-        return critiquesService;
-    }
-
-    public ProduitsService getProduitsService() {
-        return produitsService;
-    }
-
     @Autowired
     public void setProduitsService(ProduitsService produitsService) {
         this.produitsService = produitsService;
     }
-
-    public UtilisateursService getUtilisateursService() {
-        return utilisateursService;
-    }
-
-    @Autowired
-    public void setUtilisateursService(UtilisateursService utilisateursService) {
-        this.utilisateursService = utilisateursService;
-    }*/
 }
