07e911ab369eb9f0c0205b788b33e26a4710dea9
Felix-Rheaume <1934266@cegeplimoilou.ca>
2023-09-26 10:54:12 -0400

    tp1mon-16 - avancement de la vue statistique

---
+++ critique-continue-_LL_FR_AF_IH/Controlleur/src/main/java/a23/climoilou/mono2/tp1/_LL_IH_FR_AF_C/vuecontroleurs/FiltresControleur.java
@@ -0,0 +1,32 @@
+//package a23.climoilou.mono2.tp1._LL_IH_FR_AF_C.vuecontroleurs;
+//
+//import javafx.fxml.FXML;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.CheckBox;
+//import javafx.scene.control.DatePicker;
+//import net.rgielen.fxweaver.core.FxmlView;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@FxmlView("FiltresVue.fxml")
+//public class FiltresVue {
+//
+//    @FXML
+//    private CheckBox amateurCheckbox;
+//
+//    @FXML
+//    private Button confirmerButton;
+//
+//    @FXML
+//    private DatePicker debutDatePicker;
+//
+//    @FXML
+//    private CheckBox expertChecbox;
+//
+//    @FXML
+//    private DatePicker finDatePicker;
+//
+//    @FXML
+//    private CheckBox influenceurCheckbox;
+//
+//}
--- critique-continue-_LL_FR_AF_IH/Controlleur/src/main/java/a23/climoilou/mono2/tp1/_LL_IH_FR_AF_C/vuecontroleurs/StatistiquesControleur.java
+++ critique-continue-_LL_FR_AF_IH/Controlleur/src/main/java/a23/climoilou/mono2/tp1/_LL_IH_FR_AF_C/vuecontroleurs/StatistiquesControleur.java
@@ -1,15 +1,25 @@
 package a23.climoilou.mono2.tp1._LL_IH_FR_AF_C.vuecontroleurs;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.AnchorPane;
+import net.rgielen.fxweaver.core.FxControllerAndView;
+import net.rgielen.fxweaver.core.FxWeaver;
 import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 @FxmlView("StatistiqueVue.fxml")
 public class StatistiquesControleur {
+    private ConfigurableApplicationContext context;
+
+
+    @FXML
+    private Hyperlink filtreHyperlink;
 
     @FXML
     private AnchorPane toutesCritiquesPane;
@@ -34,4 +44,10 @@ public class StatistiquesControleur {
 
     }
 
+
+    public void afficherFiltres(ActionEvent actionEvent) {
+        FxWeaver fxWeaver = context.getBean(FxWeaver.class);
+        //<FiltresControleur, AnchorPane> controllerAndView = fxWeaver.load(FiltresControleur.class);
+
+    }
 }
