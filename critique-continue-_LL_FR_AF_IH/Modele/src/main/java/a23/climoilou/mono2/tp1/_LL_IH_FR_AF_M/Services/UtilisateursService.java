e8530477afb9ced6e6178f3266f6e7b45af82dbe
Felix-Rheaume <1934266@cegeplimoilou.ca>
2023-10-01 11:12:06 -0400

    Squashed commit of the following:
    commit aacf33c8aea281384e19d7093d07bdb7a826d5c9
    Author: 2166316 <2166316@cegeplimoilou.ca>
    Date:   Wed Sep 27 11:30:39 2023 -0400
    
        TP1Mon-3 et TP1Mon-4 pour les deux tickets : interfaces terminées, sauvegarde et lecture bd terminés, controleurs terminés, tests unitaire terminés
    
    commit 6704a73a932b22ddbe1e31a713fd22cb37841def
    Author: 2166316 <2166316@cegeplimoilou.ca>
    Date:   Tue Sep 26 10:59:13 2023 -0400
    
        TP1Mon13 modification création compte

--- critique-continue-_LL_FR_AF_IH/Modele/src/main/java/a23/climoilou/mono2/tp1/_LL_IH_FR_AF_M/Services/UtilisateursService.java
+++ critique-continue-_LL_FR_AF_IH/Modele/src/main/java/a23/climoilou/mono2/tp1/_LL_IH_FR_AF_M/Services/UtilisateursService.java
@@ -1,11 +1,13 @@
 package a23.climoilou.mono2.tp1._LL_IH_FR_AF_M.Services;
 
+import a23.climoilou.mono2.tp1._LL_IH_FR_AF_M.Type;
 import a23.climoilou.mono2.tp1._LL_IH_FR_AF_M.Utilisateur;
 import a23.climoilou.mono2.tp1._LL_IH_FR_AF_M.repository.Repo_Utilisateur;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +20,19 @@ public class UtilisateursService {
         this.utilisateurRepo = utilisateurRepo;
     }
 
+    public Utilisateur validationCreationUtilisateur(LocalDate dateNaissance, Type type, String nomUtilisateur, String identifiant){
+        //validation et creation d'utilisateur
+        Utilisateur utilisateur = null;
+        if(nomUtilisateur!="" && dateNaissance !=null && type!=null && identifiant!="") {
+            utilisateur = getUtilisateurRepo().findFirstByNom(identifiant);
+
+            if (utilisateur == null) {
+                utilisateur = Utilisateur.builder().nom(nomUtilisateur).type(type).dateDeNaissance(dateNaissance).identifiant(identifiant).build();
+                //applicationEventPublisher.publishEvent(new CreationCompteEvent(this,new Utilisateur()));
+            }
+        }
+        return utilisateur;
+    }
     @Transactional
     public void sauvegarderUtilisateur(Utilisateur utilisateur){
         this.utilisateurRepo.save(utilisateur);
