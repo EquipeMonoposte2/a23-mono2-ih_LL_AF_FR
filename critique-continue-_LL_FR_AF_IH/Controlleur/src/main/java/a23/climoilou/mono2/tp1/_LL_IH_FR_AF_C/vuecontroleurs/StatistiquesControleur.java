007480037e21ecb10364b1ddba78bcea25f3b0e3
Felix-Rheaume <1934266@cegeplimoilou.ca>
2023-12-03 20:18:25 -0500

    TP1MON-93 Ajustement des imports

--- critique-continue-_LL_FR_AF_IH/Controlleur/src/main/java/a23/climoilou/mono2/tp1/_LL_IH_FR_AF_C/vuecontroleurs/StatistiquesControleur.java
+++ critique-continue-_LL_FR_AF_IH/Controlleur/src/main/java/a23/climoilou/mono2/tp1/_LL_IH_FR_AF_C/vuecontroleurs/StatistiquesControleur.java
@@ -1,42 +1,31 @@
 package a23.climoilou.mono2.tp1._LL_IH_FR_AF_C.vuecontroleurs;
 
 import a23.climoilou.mono2.tp1._LL_IH_FR_AF_C.TreeViewElements.CustomListViewCell;
-import a23.climoilou.mono2.tp1._LL_IH_FR_AF_C.events.FiltresEvent;
-import a23.climoilou.mono2.tp1._LL_IH_FR_AF_M.Critique;
 import a23.climoilou.mono2.tp1._LL_IH_FR_AF_M.Items.ListItemI;
 import a23.climoilou.mono2.tp1._LL_IH_FR_AF_M.Items.UtilisateurItem;
-import a23.climoilou.mono2.tp1._LL_IH_FR_AF_M.Produit;
-import a23.climoilou.mono2.tp1._LL_IH_FR_AF_M.Services.CategorieService;
 import a23.climoilou.mono2.tp1._LL_IH_FR_AF_M.Services.DB;
 import a23.climoilou.mono2.tp1._LL_IH_FR_AF_M.Utilisateur;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import net.rgielen.fxweaver.core.FxControllerAndView;
 import net.rgielen.fxweaver.core.FxWeaver;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
